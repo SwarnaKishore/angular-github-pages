1427d0ceeca196fbc2c19687fb42521a9c22c7e4
Swarna <swarnakishoree@gmail.com>
2017-08-21 17:27:32 -0500

    Auto-generated commit

---
+++ main.bundle.js
@@ -0,0 +1,160 @@
+webpackJsonp([1,4],{
+
+/***/ 135:
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(45)();
+// imports
+exports.push([module.i, "@import url(https://fonts.googleapis.com/css?family=Signika:700,300,600);", ""]);
+
+// module
+exports.push([module.i, "html, body { height: 100%; }\r\nbody { display: -webkit-box; display: -ms-flexbox; display: flex; -webkit-box-pack: center; -ms-flex-pack: center; justify-content: center; -webkit-box-align: center; -ms-flex-align: center; align-items: center; margin:20px 0; text-align:center; background:beige; overflow:hidden; }\r\n\r\nh3 {\r\n font-size:20px;\r\n font:bold 5.5vw/1.6 'Signika', sans-serif;\r\n -webkit-user-select:none;\r\n    -moz-user-select:none;\r\n     -ms-user-select:none;\r\n         user-select:none;\r\n text-align: center;\r\n}\r\n\r\nh3 span { display:inline-block; -webkit-animation:float .2s ease-in-out infinite; animation:float .2s ease-in-out infinite; }\r\n @-webkit-keyframes float {\r\n  0%,100%{ -webkit-transform:none; transform:none; }\r\n  33%{ -webkit-transform:translateY(-1px) rotate(-2deg); transform:translateY(-1px) rotate(-2deg); }\r\n  66%{ -webkit-transform:translateY(1px) rotate(2deg); transform:translateY(1px) rotate(2deg); }\r\n}\r\n @keyframes float {\r\n  0%,100%{ -webkit-transform:none; transform:none; }\r\n  33%{ -webkit-transform:translateY(-1px) rotate(-2deg); transform:translateY(-1px) rotate(-2deg); }\r\n  66%{ -webkit-transform:translateY(1px) rotate(2deg); transform:translateY(1px) rotate(2deg); }\r\n}\r\nbody:hover span { -webkit-animation:bounce .6s; animation:bounce .6s; }\r\n@-webkit-keyframes bounce {\r\n  0%,100%{ -webkit-transform:translate(0); transform:translate(0); }\r\n  25%{ -webkit-transform:rotateX(20deg) translateY(2px) rotate(-3deg); transform:rotateX(20deg) translateY(2px) rotate(-3deg); }\r\n  50%{ -webkit-transform:translateY(-20px) rotate(3deg) scale(1.1); transform:translateY(-20px) rotate(3deg) scale(1.1);  }\r\n}\r\n@keyframes bounce {\r\n  0%,100%{ -webkit-transform:translate(0); transform:translate(0); }\r\n  25%{ -webkit-transform:rotateX(20deg) translateY(2px) rotate(-3deg); transform:rotateX(20deg) translateY(2px) rotate(-3deg); }\r\n  50%{ -webkit-transform:translateY(-20px) rotate(3deg) scale(1.1); transform:translateY(-20px) rotate(3deg) scale(1.1);  }\r\n}\r\n\r\nspan:nth-child(4n) { color:hsl(50, 75%, 55%); text-shadow:1px 1px hsl(50, 75%, 45%), 2px 2px hsl(50, 45%, 45%), 3px 3px hsl(50, 45%, 45%), 4px 4px hsl(50, 75%, 45%); }\r\nspan:nth-child(4n-1) { color:hsl(135, 35%, 55%); text-shadow:1px 1px hsl(135, 35%, 45%), 2px 2px hsl(135, 35%, 45%), 3px 3px hsl(135, 35%, 45%), 4px 4px hsl(135, 35%, 45%); }\r\nspan:nth-child(4n-2) { color:hsl(155, 35%, 60%); text-shadow:1px 1px hsl(155, 25%, 50%), 2px 2px hsl(155, 25%, 50%), 3px 3px hsl(155, 25%, 50%), 4px 4px hsl(140, 25%, 50%); }\r\nspan:nth-child(4n-3) { color:hsl(30, 65%, 60%); text-shadow:1px 1px hsl(30, 45%, 50%), 2px 2px hsl(30, 45%, 50%), 3px 3px hsl(30, 45%, 50%), 4px 4px hsl(30, 45%, 50%); }\r\n\r\nh3 span:nth-child(2){ -webkit-animation-delay:.05s; animation-delay:.05s; }\r\nh3 span:nth-child(3){ -webkit-animation-delay:.1s; animation-delay:.1s; }\r\nh3 span:nth-child(4){ -webkit-animation-delay:.15s; animation-delay:.15s; }\r\nh3 span:nth-child(5){ -webkit-animation-delay:.2s; animation-delay:.2s; }\r\nh3 span:nth-child(6){ -webkit-animation-delay:.25s; animation-delay:.25s; }\r\nh3 span:nth-child(7){ -webkit-animation-delay:.3s; animation-delay:.3s; }\r\nh3 span:nth-child(8){ -webkit-animation-delay:.35s; animation-delay:.35s; }\r\nh3 span:nth-child(9){ -webkit-animation-delay:.4s; animation-delay:.4s; }\r\nh3 span:nth-child(10){ -webkit-animation-delay:.45s; animation-delay:.45s; }\r\nh3 span:nth-child(11){ -webkit-animation-delay:.5s; animation-delay:.5s; }\r\nh3 span:nth-child(12){ -webkit-animation-delay:.55s; animation-delay:.55s; }\r\nh3 span:nth-child(13){ -webkit-animation-delay:.6s; animation-delay:.6s; }\r\nh3 span:nth-child(14){ -webkit-animation-delay:.65s; animation-delay:.65s; }\r\n\r\n\r\n\r\n", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ 136:
+/***/ (function(module, exports) {
+
+module.exports = "<h3><span>I</span><span>am</span><span>hosted</span><span>using</span><br><span>GitHub</span><span> </span><span>Pages</span></h3>\n"
+
+/***/ }),
+
+/***/ 161:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(72);
+
+
+/***/ }),
+
+/***/ 71:
+/***/ (function(module, exports) {
+
+function webpackEmptyContext(req) {
+	throw new Error("Cannot find module '" + req + "'.");
+}
+webpackEmptyContext.keys = function() { return []; };
+webpackEmptyContext.resolve = webpackEmptyContext;
+module.exports = webpackEmptyContext;
+webpackEmptyContext.id = 71;
+
+
+/***/ }),
+
+/***/ 72:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(6);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__ = __webpack_require__(78);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_app_module__ = __webpack_require__(80);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__environments_environment__ = __webpack_require__(81);
+
+
+
+
+if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["a" /* enableProdMode */])();
+}
+__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */]);
+//# sourceMappingURL=main.js.map
+
+/***/ }),
+
+/***/ 79:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(6);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var AppComponent = (function () {
+    function AppComponent() {
+        this.title = 'app works!';
+    }
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_5" /* Component */])({
+        selector: 'app-root',
+        template: __webpack_require__(136),
+        styles: [__webpack_require__(135)]
+    })
+], AppComponent);
+
+//# sourceMappingURL=app.component.js.map
+
+/***/ }),
+
+/***/ 80:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__(22);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__(6);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_forms__ = __webpack_require__(76);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_http__ = __webpack_require__(77);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__app_component__ = __webpack_require__(79);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+
+var AppModule = (function () {
+    function AppModule() {
+    }
+    return AppModule;
+}());
+AppModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_core__["b" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */]
+        ],
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
+            __WEBPACK_IMPORTED_MODULE_2__angular_forms__["a" /* FormsModule */],
+            __WEBPACK_IMPORTED_MODULE_3__angular_http__["a" /* HttpModule */]
+        ],
+        providers: [],
+        bootstrap: [__WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */]]
+    })
+], AppModule);
+
+//# sourceMappingURL=app.module.js.map
+
+/***/ }),
+
+/***/ 81:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+var environment = {
+    production: true
+};
+//# sourceMappingURL=environment.js.map
+
+/***/ })
+
+},[161]);
+//# sourceMappingURL=main.bundle.js.map
